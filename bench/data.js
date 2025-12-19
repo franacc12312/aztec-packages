b5002ab79b2101e91926e653e3e94ef2521c3e10
github-action-benchmark <github@users.noreply.github.com>
2025-12-19 18:24:26 +0000

    add Aztec Benchmarks (customSmallerIsBetter) benchmark result for 2c3e7c7e02267f9ad30e8f23d49aee6a5a9cf04f

---
+++ bench/data.js
@@ -0,0 +1,48 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1766168665463,
+  "repoUrl": "https://github.com/AztecProtocol/aztec-packages",
+  "entries": {
+    "Aztec Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "name": "AztecProtocol",
+            "username": "AztecProtocol"
+          },
+          "committer": {
+            "name": "AztecProtocol",
+            "username": "AztecProtocol"
+          },
+          "id": "2c3e7c7e02267f9ad30e8f23d49aee6a5a9cf04f",
+          "message": "fix: nightly spartan benchmarks",
+          "timestamp": "2025-12-19T17:19:18Z",
+          "url": "https://github.com/AztecProtocol/aztec-packages/pull/19140/commits/2c3e7c7e02267f9ad30e8f23d49aee6a5a9cf04f"
+        },
+        "date": 1766168595629,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "spartan/ci/network_deploy/total",
+            "value": 569,
+            "unit": "seconds"
+          },
+          {
+            "name": "spartan/ci/network_deploy/eth_devnet",
+            "value": 144,
+            "unit": "seconds"
+          },
+          {
+            "name": "spartan/ci/network_deploy/rollup_contracts",
+            "value": 64,
+            "unit": "seconds"
+          },
+          {
+            "name": "spartan/ci/network_deploy/aztec_infra",
+            "value": 239,
+            "unit": "seconds"
+          }
+        ]
+      }
+    ]
+  }
+}
