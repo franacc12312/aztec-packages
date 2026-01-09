04bd5bf3ac6f36f9c7fbf9aa6c7ca15f144918d5
Nikita Meshcheriakov <root@nikitam.io>
2026-01-09 18:12:13 -0300

    add missing file

---
+++ yarn-project/p2p/src/msg_validators/tx_validator/size_validator.ts
@@ -0,0 +1,17 @@
+import { createLogger } from '@aztec/foundation/log';
+import { Tx, type TxValidationResult, type TxValidator } from '@aztec/stdlib/tx';
+
+export class SizeTxValidator implements TxValidator<Tx> {
+  #log = createLogger('sequencer:tx_validator:tx_size');
+  constructor(private readonly maxSize: number) {}
+  validateTx(tx: Tx): Promise<TxValidationResult> {
+    const txSize = tx.getSize();
+    if (txSize > this.maxSize) {
+      this.#log.verbose(
+        `Rejecting transaction ${tx.getTxHash().toString()}. Reason: size above size limit. ${txSize} > ${this.maxSize}`,
+      );
+      return Promise.resolve({ result: 'invalid', reason: ['Transaction size above size limit'] });
+    }
+    return Promise.resolve({ result: 'valid' });
+  }
+}
